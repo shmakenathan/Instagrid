6fcacee9773d57f0cd8453b2d1e3cbb56b363a60
Nathan <nathan.chicha.openclassroom@gmail.com>
2019-11-29 13:37:56 +0100

    ajout fonctionnalite share

--- Instagrid/InstagridViewController.swift
+++ Instagrid/InstagridViewController.swift
@@ -11,15 +11,67 @@ import UIKit
 class InstagridViewController: UIViewController {
 
     @IBOutlet weak var bottomRightView: UIView!
+    
+    @IBOutlet weak var upRightView: UIView!
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var arrow: UIImageView!
+    @IBOutlet weak var selectLayout1: UIImageView!
+    
+    @IBOutlet weak var selectLayout2: UIImageView!
+    
+    @IBOutlet weak var selectLayout3: UIImageView!
+    
+    
+    
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func swipeUp(_ sender: Any) {
+        
+        shareTapped()
+        
+        
+    }
+    
+    @IBAction func layout1ButtonClick(_ sender: Any) {
+        self.upRightView.isHidden = true
+        self.bottomRightView.isHidden = false
+        self.selectLayout1.isHidden = false
+        self.selectLayout2.isHidden = true
+        self.selectLayout3.isHidden = true
+    }
+    
+    @IBAction func layout2ButtonClick(_ sender: Any) {
+        self.upRightView.isHidden = false
+        self.bottomRightView.isHidden = true
+        self.selectLayout1.isHidden = true
+        self.selectLayout2.isHidden = false
+        self.selectLayout3.isHidden = true
+    }
+    
     @IBAction func layout3ButtonClick(_ sender: UIButton) {
         self.bottomRightView.isHidden = false
+        self.upRightView.isHidden = false
+        self.selectLayout1.isHidden = true
+        self.selectLayout2.isHidden = true
+        self.selectLayout3.isHidden = false
+    }
+    
+    @objc func shareTapped() {
+        let vc = UIActivityViewController(activityItems: [imageView.image!], applicationActivities: [])
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(vc, animated: true)
     }
     /*
     // MARK: - Navigation
